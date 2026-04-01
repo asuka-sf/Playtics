eb9e89b2dd223c43f8c721f6237550acf81880fe
Asuka Suehiro <asukan1118@gmail.com>
2026-04-01 11:50:53 -0700

    ✅ test: add unit tests for get player usecase

--- internal/usecase/player_usecase_test.go
+++ internal/usecase/player_usecase_test.go
@@ -21,6 +21,19 @@ func (m *MockPlayerRepository) Create(ctx context.Context, player *domain.Player
 	return m.player, m.err
 }
 
+func (m *MockPlayerRepository) FindByID(ctx context.Context, id uuid.UUID) (*domain.Player, error) {
+	return m.player, m.err
+}
+
+var playerResult = &domain.Player{
+	ID:        uuid.MustParse("550e8400-e29b-41d4-a716-446655440000"),
+	Name:      "Alice",
+	Email:     "alice@email.com",
+	ImageURL:  "test_image.jpg",
+	CreatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
+	UpdatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
+}
+
 func TestCreatePlayer(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -35,15 +48,8 @@ func TestCreatePlayer(t *testing.T) {
 				Email:    "alice@email.com",
 				ImageURL: "test_image.jpg",
 			},
-			result: &domain.Player{
-				ID:        uuid.MustParse("550e8400-e29b-41d4-a716-446655440000"),
-				Name:      "Alice",
-				Email:     "alice@email.com",
-				ImageURL:  "test_image.jpg",
-				CreatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
-				UpdatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
-			},
-			err: nil,
+			result: playerResult,
+			err:    nil,
 		},
 		{
 			name: "success: no image_url",
@@ -52,15 +58,8 @@ func TestCreatePlayer(t *testing.T) {
 				Email:    "alice@email.com",
 				ImageURL: "",
 			},
-			result: &domain.Player{
-				ID:        uuid.MustParse("550e8400-e29b-41d4-a716-446655440000"),
-				Name:      "Alice",
-				Email:     "alice@email.com",
-				ImageURL:  "",
-				CreatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
-				UpdatedAt: time.Date(2026, 3, 30, 10, 0, 0, 0, time.UTC),
-			},
-			err: nil,
+			result: playerResult,
+			err:    nil,
 		},
 		{
 			name: "failed to create player",
@@ -84,6 +83,50 @@ func TestCreatePlayer(t *testing.T) {
 			player, err := uc.Create(ctx, tt.args.Name, tt.args.Email, tt.args.ImageURL)
 
 			// check if the result and error has the values as expected
+			if tt.err != nil {
+				assert.Error(t, err)
+				assert.Nil(t, player)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.result, player)
+			}
+		})
+	}
+}
+
+func TestGetPlayer(t *testing.T) {
+	tests := []struct {
+		name   string
+		result *domain.Player
+		err    error
+	}{
+		{
+			name:   "success",
+			result: playerResult,
+			err:    nil,
+		},
+		{
+			name: "player not found",
+			err:  domain.ErrNotFound,
+		},
+		{
+			name: "internal server error",
+			err:  errors.New("connection refused"),
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mock := &MockPlayerRepository{
+				player: tt.result,
+				err:    tt.err,
+			}
+			uc := NewPlayerUsecase(mock)
+			id := uuid.MustParse("550e8400-e29b-41d4-a716-446655440000")
+			player, err := uc.GetByID(ctx, id)
+
 			if tt.err != nil {
 				assert.Error(t, err)
 				assert.Nil(t, player)
